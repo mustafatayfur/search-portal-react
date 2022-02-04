0061a5b9bfb3d2d80d7617040eb4689c82a8dcae
mustafa <mltm0204@gmail.com>
2022-02-04 10:20:58 +0300

    fixed

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -17,13 +17,23 @@ const Pagination = ({paginate,output,currentPage,cardPerPage}) => {
                         Prev
                     </a>
                 </li>
-            {pageNumbers.map(number => (
+            {pageNumbers.slice(0,3).map(number => (
                 <li key = {number} className = "page-item">
                     <a  onClick={() => paginate(number)} href = "#" className={`page-btn ${number === currentPage && 'active-btn'}`}>
                         {number}
                     </a>
                 </li>
             ))}
+                <li className = {pageNumbers.length > 6 ? "page-item" : "hidden"}>
+                    ...
+                </li>
+                {pageNumbers.slice(pageNumbers.length-3,pageNumbers.length).map(number => (
+                    <li key = {number} className = {pageNumbers.length > 6 ? "page-item" : "hidden"}>
+                        <a  onClick={() => paginate(number)} href = "#" className={`page-btn ${number === currentPage && 'active-btn'}`}>
+                            {number}
+                        </a>
+                    </li>
+                ))}
                 <li className = "page-item">
                     <a  onClick={() => paginate(currentPage + 1)} href = "#" className='page-btn'>
                      Next
